00fa48d67050312a0c6e1c31cdb3c1d9a7e0ed7c
Cooper Gillan <cooper.gillan@gmail.com>
2021-02-26 22:44:09 -0600

    Day 2 Part 2 answers (#3)
    What
    
    Split the two parts into separate modules to separate the two rules for
    validating passwords.
    
    Why
    
    This allows functionality shared between the two parts of day two to be
    in the main module, then the different functionality, namely validating
    the passwords, to be split into separate modules.
    
    There is some repeated code since it wasn't possible at the time to pass
    the InputReader into a possible shared checker :man_shrugging:

--- day_2/day_2.cr
+++ day_2/day_2.cr
@@ -6,7 +6,7 @@ require "path"
 FILE_PATH = Path.new(Dir.current, "day_2", "source_data.txt")
 
 module Day2
-  class InputValidator
+  class InputReader
     def initialize(input : String)
       @lower_bound = 0
       @upper_bound = 0
@@ -38,34 +38,90 @@ module Day2
     def password
       @password
     end
+  end
 
-    def password_valid?
-      @password.count(@letter) <= @upper_bound && @password.count(@letter) >= @lower_bound
+  module Part1
+    class Checker
+      property valid_passwords
+      property input_file : Path
+      # property input_reader : Day2::InputReader # Couldn't get this to work
+      # This would mean we wouldn't need to repeat the initialize method
+      # It resulted in this error:
+      #
+      # Error: instance variable '@validator' of Day2::Checker must be Day2::InputReader+, not Day2::InputReader.class
+
+      def initialize(@input_file)
+        @valid_passwords = 0
+      end
+
+      def check_input
+        File.open(input_file) do |file|
+          file.each_line do |line|
+            input_reader = Day2::Part1::InputReader.new(line)
+            if input_reader.password_valid?
+              @valid_passwords += 1
+            end
+          end
+        end
+        puts "Found #{valid_passwords} valid passwords for part 1 in #{input_file}"
+      end
+    end
+
+    class InputReader < Day2::InputReader
+      def password_valid?
+        @password.count(@letter) <= @upper_bound && @password.count(@letter) >= @lower_bound
+      end
     end
   end
 
-  class Checker
-    property valid_passwords
-    property input_file : Path
+  module Part2
+    class InputReader < Day2::InputReader
+      def first_matches?
+      index = @lower_bound - 1
+      @password.byte_at(index) == @letter.byte_at(0)
+      end
 
-    def initialize(@input_file)
-      @valid_passwords = 0
-    end
+      def second_matches?
+        index = @upper_bound - 1
+        @password.byte_at(index) == @letter.byte_at(0)
+      end
 
-    def check_input
-      File.open(input_file) do |file|
-        file.each_line do |line|
-          validator = InputValidator.new(line)
-          if validator.password_valid?
-            @valid_passwords += 1
-          end
+      def password_valid?
+        if (!first_matches? && !second_matches?) || (first_matches? && second_matches?)
+          return false
+        end
+        if first_matches? || second_matches?
+          return true
         end
       end
-      puts "Found #{valid_passwords} valid passwords in #{input_file}"
+    end
+
+    class Checker
+      property valid_passwords
+      property input_file : Path
+
+      def initialize(@input_file)
+        @valid_passwords = 0
+      end
+
+      def check_input
+        File.open(input_file) do |file|
+          file.each_line do |line|
+            input_reader = Day2::Part2::InputReader.new(line)
+            if input_reader.password_valid?
+              @valid_passwords += 1
+            end
+          end
+        end
+        puts "Found #{valid_passwords} valid passwords for part 2 in #{input_file}"
+      end
     end
   end
 end
 
 # main function
-main_checker = Day2::Checker.new(FILE_PATH)
-main_checker.check_input
+part1_main_checker = Day2::Part1::Checker.new(FILE_PATH)
+part1_main_checker.check_input
+
+part2_main_checker = Day2::Part2::Checker.new(FILE_PATH)
+part2_main_checker.check_input
